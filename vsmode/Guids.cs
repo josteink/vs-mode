24bcaf99f866a62b88c8a521fe4c907e6ba45b34
Jostein Kjønigsen <jostein@kjonigsen.net>
2015-07-13 15:50:54 +0200

    Initial commit - Code scaffolded by VS.

---
+++ vsmode/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace kjonigsennet.vsmode
+{
+    static class GuidList
+    {
+        public const string guidvsmodePkgString = "d83eb81c-8121-4fe7-9f9f-9e15a0e9836a";
+        public const string guidvsmodeCmdSetString = "237da2f0-6aa6-435f-b291-0e9efbe62ef9";
+
+        public static readonly Guid guidvsmodeCmdSet = new Guid(guidvsmodeCmdSetString);
+    };
+}
---
+++ vsmode/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace kjonigsennet.vsmode
+{
+    static class PkgCmdIDList
+    {
+        public const uint vsmodeOpenInEmacs =        0x100;
+
+
+    };
+}
